1ae91efc315f7240d2486685bff24064cf424f79
Szymon Roszyk <roszyk.szymon@gmail.com>
2022-05-23 21:01:41 +0200

    Excceptions

---
+++ CatchExceptions/Zadanie1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    /// <summary>
+    /// Bez uruchamiania programu zastanów się jaki wynik będzie wyświetlony na konsoli
+    /// </summary>
+    public class Zadanie1
+    {
+        public static void Test()
+        {
+            try
+            {
+                throw new ArgumentNullException();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("ArgumentException");
+                throw ex;
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine("NotImplementedException");
+                throw;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Koniec");
+            }
+
+            Console.WriteLine("Tutaj też koniec");
+        }
+    }
+}
---
+++ CatchExceptions/Zadanie2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    /// <summary>
+    /// Zmodyfikuj kod w taki sposób aby program nie zakończył swojego działania z błędem - trzeba złapać wyjątek oraz żeby w FinalFunkcja dostępny był obiekt wyjątku z poprawnym StackTracem - właściwość ex.StackTrace
+    /// </summary>
+    public class Zadanie2
+    {
+        public void Funckja1()
+        {
+            throw new NotImplementedException("Funckja1");
+        }
+
+        public void Funkcj2()
+        {
+            Funckja1();
+        }
+
+        public void Funkcj3()
+        {
+            Funkcj2();
+        }
+
+        public void Funkcj4()
+        {
+            Funkcj3();
+        }
+
+        public void FinalFunkcja()
+        {
+            Funkcj4();
+        }
+
+    }
+}
