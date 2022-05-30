fb80767397be6dad513b27062153a95138e26f3b
Szymon Roszyk <roszyk.szymon@gmail.com>
2022-05-30 18:14:07 +0200

    fix

---
+++ EF/EFKasia/Program.cs
@@ -0,0 +1,29 @@
+﻿using EFKasia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFKasia
+{
+    internal class Program
+    {
+        static string ToLoweCaseLetter(string text)
+        {
+            return text.ToLower();
+        }
+
+        static void Main(string[] args)
+        {
+            using (var ctx = new FirstDbContext())
+            {
+                var device = ctx.Devices.FirstOrDefault();
+                device.Name = "NewName";
+                ctx.SaveChanges();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
